1578227b13ab5820992ce2e93e55c26fe19a7e67
data-team-ismart <dados@ismart.org.br>
2025-05-20 13:04:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import time
 st.set_page_config(page_title = "Dash Alumni",  initial_sidebar_state="collapsed", page_icon='🎓', layout = "wide")
 
 
-leia_me = st.Page("pages/leia_me.py", title="Leia-me", icon='🧾', layout = "wide")
-dados_gerais = st.Page("pages/alumni_dash_dados_gerais.py", title="Dados Gerais", icon='📈', layout = "wide")
-engajamento = st.Page("pages/alumni_dash_engajamento.py", title="Engajamento e Giveback", icon='🎯', layout = "wide")
-mentoria = st.Page("pages/alumni_dash_mentoria.py", title="Mentoria", icon='🧑‍🏫', layout = "wide")
-download = st.Page("pages/alumni_dash_downloads_bases.py", title="Download de bases",icon='💾', layout = "wide")
+leia_me = st.Page("pages/leia_me.py", title="Leia-me", icon='🧾')
+dados_gerais = st.Page("pages/alumni_dash_dados_gerais.py", title="Dados Gerais", icon='📈')
+engajamento = st.Page("pages/alumni_dash_engajamento.py", title="Engajamento e Giveback", icon='🎯')
+mentoria = st.Page("pages/alumni_dash_mentoria.py", title="Mentoria", icon='🧑‍🏫')
+download = st.Page("pages/alumni_dash_downloads_bases.py", title="Download de bases",icon='💾')
 
 
 
