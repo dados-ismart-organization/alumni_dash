f74d6ea3751ef9d2ffc4f7e9378bcb9ccecf8961
data-team-ismart <dados@ismart.org.br>
2025-05-20 12:59:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from utils.auth import *
 import time
 
 
-st.set_page_config(page_title = "Dash Alumni",  initial_sidebar_state="expanded", page_icon='🎓')
+st.set_page_config(page_title = "Dash Alumni",  initial_sidebar_state="expanded", page_icon='🎓', layout = "wide")
 
-leia_me = st.Page("pages/leia_me.py", title="Leia-me", icon='🧾', layout=wide)
-dados_gerais = st.Page("pages/alumni_dash_dados_gerais.py", title="Dados Gerais", icon='📈', layout=wide)
-engajamento = st.Page("pages/alumni_dash_engajamento.py", title="Engajamento e Giveback", icon='🎯', layout=wide)
-mentoria = st.Page("pages/alumni_dash_mentoria.py", title="Mentoria", icon='🧑‍🏫', layout=wide)
-download = st.Page("pages/alumni_dash_downloads_bases.py", title="Download de bases",icon='💾', layout=wide)
+
+leia_me = st.Page("pages/leia_me.py", title="Leia-me", icon='🧾', layout = "wide")
+dados_gerais = st.Page("pages/alumni_dash_dados_gerais.py", title="Dados Gerais", icon='📈', layout = "wide")
+engajamento = st.Page("pages/alumni_dash_engajamento.py", title="Engajamento e Giveback", icon='🎯', layout = "wide")
+mentoria = st.Page("pages/alumni_dash_mentoria.py", title="Mentoria", icon='🧑‍🏫', layout = "wide")
+download = st.Page("pages/alumni_dash_downloads_bases.py", title="Download de bases",icon='💾', layout = "wide")
 
 
 
